ee9ec9ad2d34294d95c030ed59368ccfcb4a0b9c
Jinne9 <paradice29@naver.com>
2020-10-01 22:07:46 +0900

    Updates

---
+++ precache-manifest.26c513da9ec44d65d9900161a2df794b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba27054bb2d8cd96c8e28c771522672",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "c0db874fa340fe4a42e9",
+    "url": "/movie_app_2020/static/css/main.33e3e186.chunk.css"
+  },
+  {
+    "revision": "a7d22b2c00d8d326c863",
+    "url": "/movie_app_2020/static/js/2.300fc6d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.300fc6d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0db874fa340fe4a42e9",
+    "url": "/movie_app_2020/static/js/main.24e35dfc.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
